a797034beb56ef3e491dd0cda989f2e06e07dd5b
OlhaKatrych <olgak198931@gmail.com>
2024-01-08 12:45:31 +0200

    2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,27 +1,44 @@
 const form = document.querySelector('.feedback-form');
 const LS_KEY = 'feedback-form-state';
-const input = form.elements.email;
-const textarea = form.elements.message;
+const input = form.querySelector('input');
+const textarea = form.querySelector('textarea');
 
 form.addEventListener('input', handlerInput);
 form.addEventListener('submit', handlerSubmit);
 
+getItems()
+
 function handlerInput(e) {
-  const LS_OBJ = {
-    email: localStorage.setItem(LS_KEY, e.target.value.trim()),
-    message: localStorage.setItem(LS_KEY, e.target.value.trim()),
+  const objInputValue = {
+    email: e.target.value.trim(),
+    message: e.target.value.trim(),
   };
+
+  localStorage.setItem(LS_KEY, JSON.stringify(objInputValue));
 }
 
 function handlerSubmit(e) {
   e.preventDefault();
-  const LS_OBJ = {
+  const elements = {
     email: e.target.elements.email.value,
     message: e.target.elements.message.value,
   };
-  input.value = localStorage.getItem(LS_KEY) ?? '';
-  textarea.value = localStorage.getItem(LS_KEY) ?? '';
-  console.log(LS_OBJ);
+
+  console.log(elements);
   localStorage.removeItem(LS_KEY);
   form.reset();
 }
+
+function getItems() {
+  const savedMessage = localStorage.getItem(LS_KEY);
+
+  if (savedMessage) {
+    input.value = savedMessage;
+    textarea.value = savedMessage;
+  }else{
+
+    input.value = '';
+    textarea.value = '';
+  }
+}
+
