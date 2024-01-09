4d3097b1e2c618260fe961425363b7b072c91b76
OlhaKatrych <olgak198931@gmail.com>
2024-01-09 11:03:32 +0200

    2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,9 +1,18 @@
 const form = document.querySelector('.feedback-form');
 const LS_KEY = 'feedback-form-state';
 const input = form.elements.email;
-input.value = localStorage.getItem(LS_KEY) ?? '';
 const message = form.elements.message;
-message.value = localStorage.getItem(LS_KEY) ?? '';
+
+getDataFromLS();
+
+function getDataFromLS() {
+  const dataFromLS = localStorage.getItem(LS_KEY);
+  if (dataFromLS) {
+    const savedParse = JSON.parse(dataFromLS);
+    input.value = savedParse.email ?? '';
+    message.value = savedParse.message ?? '';
+  }
+}
 
 form.addEventListener('input', handlerInput);
 form.addEventListener('submit', handlerSubmit);
